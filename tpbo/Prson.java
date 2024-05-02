4fcecd1952309eab4c18b168cc4a002c64b01e6c
faishal55 <128132873+faishal55@users.noreply.github.com>
2024-05-02 22:11:16 +0700

    Add files via upload

---
+++ tpbo/Prson.java
@@ -0,0 +1,26 @@
+package tpbo;
+
+
+class Person {
+    String name;
+    String address;
+    String hobi;
+
+    public Person(String name, String address, String hobi) {
+        this.name = name;
+        this.address = address;
+        this.hobi = hobi;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHobi() {
+        return hobi;
+    }
+}
